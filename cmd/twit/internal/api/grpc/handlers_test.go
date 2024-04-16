b7e898a3204064edc7971654accb681b7e059473
FarGao <Knifechik@gmail.com>
2024-04-16 18:45:56 +0300

    updated test

--- cmd/twit/internal/api/grpc/handlers_test.go
+++ cmd/twit/internal/api/grpc/handlers_test.go
@@ -89,3 +89,48 @@ func TestApi_TwitPost(t *testing.T) {
 		})
 	}
 }
+
+func TestApi_TwitUpdate(t *testing.T) {
+	t.Parallel()
+
+	var (
+		id       = uuid.Must(uuid.NewV4())
+		authorId = uuid.Must(uuid.NewV4())
+		text     = "ya ne hochu etogo govna"
+		appRes   = &app.Twit{ID: id, AuthorID: authorId, Text: text}
+		res      = &twit_pb.TwitUpdateResponse{
+			Twit: &twit_pb.Twit{
+				Id: id.String(), AuthorId: authorId.String(), Text: text}}
+		errInternal = status.Error(codes.Internal, fmt.Sprintf("a.app.TwitUpdate: %s", errAny))
+	)
+
+	testCases := map[string]struct {
+		id      uuid.UUID
+		appRes  *app.Twit
+		text    string
+		appErr  error
+		want    *twit_pb.TwitUpdateResponse
+		wantErr error
+	}{
+		"success":          {id, appRes, text, nil, res, nil},
+		"a.app.TwitUpdate": {id, appRes, text, errAny, nil, errInternal},
+	}
+
+	for name, tc := range testCases {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx, c, mockApp, assert := start(t, dom.UserStatusDefault)
+
+			mockApp.EXPECT().TwitUpdate(gomock.Any(), session, tc.id, tc.text).Return(tc.appRes, tc.appErr)
+
+			resp, err := c.TwitUpdate(auth(ctx), &twit_pb.TwitUpdateRequest{
+				Id:   tc.id.String(),
+				Text: tc.text,
+			})
+			assert.ErrorIs(err, tc.wantErr)
+			assert.True(proto.Equal(resp, tc.want))
+		})
+	}
+}
